d7d303566ef8a9ed16afea9cab278b82b236f26a
pablonyaaUwu2022 <pabyiart28@gmail.com>
2026-02-15 00:18:47 -0600

    Lexer con identificadores, numeros, operadores y cadenas

---
+++ workspace/examples/pablo/prueba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 12.5;
+    double b = 4e3;
+    int hex = 0xAB;
+
+    if(a <= b || hex != 0){
+        cout << "Resultado\n";
+    }
+}
